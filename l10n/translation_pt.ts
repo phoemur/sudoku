ef4f768b7b8f41326b05a6346bbcc77fdf4b3a93
phoemur@gmail.com <phoemur@gmail.com>
2020-01-01 15:30:57 -0300

    Add PT translation

---
+++ l10n/translation_pt.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Janela Principal</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="35"/>
+        <source>&amp;File</source>
+        <translation>&amp;Arquivo</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="52"/>
+        <source>&amp;Help</source>
+        <translation>A&amp;juda</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="65"/>
+        <source>&amp;New Game</source>
+        <translation>&amp;Novo Jogo</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="68"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="73"/>
+        <source>&amp;Beginner</source>
+        <translation>&amp;Iniciante</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="78"/>
+        <source>&amp;Intermediate</source>
+        <translation>Inter&amp;mediário</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="83"/>
+        <source>&amp;Expert</source>
+        <translation>&amp;Difícil</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="92"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="95"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="107"/>
+        <source>&amp;About</source>
+        <translation>&amp;Sobre</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="110"/>
+        <source>Ctrl+H</source>
+        <translation>Ctrl+H</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="115"/>
+        <source>&amp;Solve</source>
+        <translation>&amp;Resolver</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="32"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="33"/>
+        <source>Sudoku version 0.1
+Author: Fernando B. Giannasi
+jan/2020</source>
+        <translation>Sudoku versão 0.1
+Autor: Fernando B. Giannasi
+janeiro/2020</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="69"/>
+        <source>Congratulations!</source>
+        <translation>Parabéns!</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="70"/>
+        <source>You won!</source>
+        <translation>Você Venceu!</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <QApplication>
+#include <QTranslator>
 #include <mainwindow.h>
+
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
     QApplication app (argc, argv);
+
+    QTranslator translator;
+
+    // Find our locale and check if there is a translation available
+    QString defaultLocale = QLocale::system().name();
+    defaultLocale.truncate(defaultLocale.lastIndexOf('_'));
+
+    if (translator.load(QString("translation_") + defaultLocale))
+            app.installTranslator(&translator);
+
+    //Run
     MainWindow win;
     win.setWindowTitle("Sudoku");
     win.move(200, 200);
